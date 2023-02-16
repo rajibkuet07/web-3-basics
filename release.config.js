f592ac31a6c4c6c377980d2c86213ebffa158784
Rajib Dey <rajib.kuet07@gmail.com>
2023-02-17 00:31:56 +0600

    ci: add changelog plugin in release config

--- release.config.js
+++ release.config.js
@@ -4,13 +4,19 @@ const config = {
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
 		[
+			'@semantic-release/changelog',
+			{
+				changelogFile: 'CHANGELOG.md',
+			},
+		],
+		'@semantic-release/github',
+		[
 			'@semantic-release/git',
 			{
 				message:
 					'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
 			},
 		],
-		'@semantic-release/github',
 	],
 };
 
