0364870b4688337e42f987305fe4663483b59d42
Rajib Dey <rajib.kuet07@gmail.com>
2023-02-16 20:45:44 +0600

    ci: add autmated versioning
    Add semantic-release to automate the release versioning.
    Add the necessary configuration for github actions.

---
+++ release.config.js
@@ -0,0 +1,17 @@
+const config = {
+	branches: ['master'],
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		[
+			'@semantic-release/git',
+			{
+				message:
+					'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+			},
+		],
+		'@semantic-release/github',
+	],
+};
+
+module.exports = config;
